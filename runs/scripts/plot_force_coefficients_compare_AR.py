f80520de49a16c23a43ccef3b5c47b729b11457f
mesnardo <mesnardo@gwu.edu>
2020-04-17 11:24:32 -0400

    Update forces figure with results for largest AR

--- runs/scripts/plot_force_coefficients_compare_AR.py
+++ runs/scripts/plot_force_coefficients_compare_AR.py
@@ -27,6 +27,15 @@ solution2 = rodney.load_force_coefficients(filepath, config2)
 rodney.print_stats(label2, *rodney.get_stats(solution2, limits=(3, 5)))
 plot_kwargs2 = dict(color='black', linestyle='--')
 
+# Load force coefficients for simulation with AR=2.55.
+label3 = 'AR = 2.55'
+simudir3 = maindir / 'Re200_St0.6_AR2.55_psi90'
+config3 = rodney.WingKinematics(Re=200, AR=2.55, nt_period=2000)
+filepath = simudir3 / 'output' / 'forces-0.txt'
+solution3 = rodney.load_force_coefficients(filepath, config3)
+rodney.print_stats(label3, *rodney.get_stats(solution3, limits=(3, 5)))
+plot_kwargs3 = dict(color='C0', linestyle='-.')
+
 # Plot the history of the force coefficients.
 pyplot.rc('font', family='serif', size=12)
 fig, (ax1, ax2, ax3) = pyplot.subplots(ncols=3, figsize=(12.0, 3.0))
@@ -36,6 +45,7 @@ ax1.set_xlabel('$t / T$')
 ax1.set_ylabel('$C_T$')
 ax1.plot(solution.t, solution.ct, label=label, **plot_kwargs)
 ax1.plot(solution2.t, solution2.ct, label=label2, **plot_kwargs2)
+ax1.plot(solution3.t, solution3.ct, label=label3, **plot_kwargs3)
 ax1.set_xlim(xlim)
 ax1.set_ylim(ylim)
 # Plot the history of the lift coefficient.
@@ -43,6 +53,7 @@ ax2.set_xlabel('$t / T$')
 ax2.set_ylabel('$C_L$')
 ax2.plot(solution.t, solution.cl, label=label, **plot_kwargs)
 ax2.plot(solution2.t, solution2.cl, label=label2, **plot_kwargs2)
+ax2.plot(solution3.t, solution3.cl, label=label3, **plot_kwargs3)
 ax2.set_xlim(xlim)
 ax2.set_ylim(ylim)
 # Plot the history of the spanwise force coefficient.
@@ -50,6 +61,7 @@ ax3.set_xlabel('$t / T$')
 ax3.set_ylabel('$C_Z$')
 ax3.plot(solution.t, solution.cz, label=label, **plot_kwargs)
 ax3.plot(solution2.t, solution2.cz, label=label2, **plot_kwargs2)
+ax3.plot(solution3.t, solution3.cz, label=label3, **plot_kwargs3)
 ax3.set_xlim(xlim)
 ax3.set_ylim(ylim)
 
